add3aec3b0b1f670cc9600a1bdbcc41ae4c2fc95
matheusnicolas <matheuspb8@gmail.com>
2020-09-03 14:09:01 -0300

    added ghost IA and fixed environment

---
+++ Assets/Scripts/WaypointPatrol.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WaypointPatrol : MonoBehaviour
+{   
+
+    public NavMeshAgent navMeshAgent;
+    public Transform[] waypoints;
+    public int currentWaypointIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent.SetDestination(waypoints[0].position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (navMeshAgent.remainingDistance < navMeshAgent.stoppingDistance) 
+        {
+            currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Length;
+            navMeshAgent.SetDestination(waypoints[currentWaypointIndex].position);
+        }
+    }
+}
